3199746923b4be40f532ab9d51c5aae97e16f566
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-09-24 15:48:28 -0500

    Added gulp/jshint support and questions/answers data structure

---
+++ src/DynamicQuiz/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+var paths = {
+    src: "./wwwroot/js/*js",
+}
+
+gulp.task('default', function () {
+    // place code for your default task here
+    return gulp.src(paths.src)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'))
+});
--- src/DynamicQuiz/wwwroot/js/app.js
+++ src/DynamicQuiz/wwwroot/js/app.js
@@ -1,1 +1,22 @@
-﻿// Only one question is in this array, but you will add all the questions.var allQuestions = [{question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}]
+﻿// Only one question is in this array, but you will add all the questions.
+
+var allQuestions = [
+    {
+        question: "How many days are in a week?",
+        choices: ["1", "10", "5", "7"],
+        correctAnswer: 3
+    },
+    {
+        question: "Hoy many days are in a year?",
+        choices: ["423", "365", "345", "234"],
+        correctAnswer: 1
+    },
+    {
+        question: "Hoy many minutes are in an hour?",
+        choices: ["60", "34", "77", "20"],
+        correctAnswer: 0
+    },
+]
+
+var nCorrectAnswer = 0;
+var iQuestion = 0;
