cc66cc2602b8c2f7e69250e9e00afc90e153aaf2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-09-27 23:35:50 -0500

    Added Kestrel server and more client code

--- src/DynamicQuiz/wwwroot/js/app.js
+++ src/DynamicQuiz/wwwroot/js/app.js
@@ -20,3 +20,32 @@ var allQuestions = [
 
 var nCorrectAnswer = 0;
 var iQuestion = 0;
+
+var buttonNode = document.getElementById('quizNextBtn');
+buttonNode.addEventListener('click', function (event) {
+    var prompt = document.getElementById('quizPrompt');
+    if (iQuestion < allQuestions.length) {
+        var question = allQuestions[iQuestion];
+        prompt.textContent = question.question;
+        var choicesNode = this.previousElementSibling();
+
+        for (var i = 0; i < question.choices.length; i++) {
+            alert(i);
+            var input = document.createElement('input')
+            input.type = 'radio';
+            input.name = 'question';
+            input.value = i.toString();
+            var choice = document.createElement('text');
+            choice.textContent = question.choices[i];
+            input.appendChild(choice);
+            choicesNode.appendChild(input);
+        }
+
+        this.text = "Next";
+        iQuestion++;
+    } else {
+        prompt.textContent = "You finished the quiz, your score is "
+            + nCorrectAnswer + "/" + allQuestions.length;
+        this.style.display = 'none';
+    }
+});
