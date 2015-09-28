3cd8d9812a75e885550d433ed8cb4c76e70665e2
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-09-28 02:34:43 -0500

    Implemented correct evaluation of answers

--- src/DynamicQuiz/wwwroot/js/app.js
+++ src/DynamicQuiz/wwwroot/js/app.js
@@ -1,6 +1,4 @@
-﻿// Only one question is in this array, but you will add all the questions.
-
-var allQuestions = [
+﻿var allQuestions = [
     {
         question: "How many days are in a week?",
         choices: ["1", "10", "5", "7"],
@@ -19,19 +17,30 @@ var allQuestions = [
 ]
 
 var nCorrectAnswer = 0;
-var iQuestion = 0;
+var iQuestion = -1;
 
 var buttonNode = document.getElementById('quizNextBtn');
 buttonNode.addEventListener('click', function (event) {
     var prompt = document.getElementById('quizPrompt');
-    var choicesNode = this.previousElementSibling;
+
+    if (iQuestion >= 0) {
+        var radios = this.parentElement.getElementsByTagName('input');
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
+                break;
+            }
+        }
+        nCorrectAnswer = i === allQuestions[iQuestion].correctAnswer
+            ? nCorrectAnswer + 1 : nCorrectAnswer;
+    }
 
     // Remove all div child nodes
     while (choicesNode.firstChild) {
         choicesNode.removeChild(choicesNode.firstChild);
     }
 
-    if (iQuestion < allQuestions.length) {
+    if (iQuestion < allQuestions.length - 1) {
+        iQuestion++;
         this.disabled = true;
         var question = allQuestions[iQuestion];
         prompt.textContent = question.question;
@@ -46,12 +55,11 @@ buttonNode.addEventListener('click', function (event) {
             choicesNode.appendChild(document.createTextNode(question.choices[i]));
             choicesNode.appendChild(document.createElement('br'));
         }
-
         this.text = "Next";
-        iQuestion++;
     } else {
         prompt.textContent = "You finished the quiz, your score is "
             + nCorrectAnswer + "/" + allQuestions.length;
+        this.disabled = true;
         this.style.display = 'none';
     }
 });
